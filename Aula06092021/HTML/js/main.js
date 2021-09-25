c870b91935cb33bf6a0bd62fc205b82da04486bb
Marlus <antos2lite@gmail.com>
2021-09-25 15:31:05 -0300

    agora o pronto liberar

--- Aula06092021/HTML/js/main.js
+++
@@ -1,9 +0,0 @@
-function light(sw) {
-    var pic;
-    if (sw == 0) {
-      pic = "img/pic_bulboff.gif"
-    } else {
-      pic = "img/pic_bulbon.gif"
-    }
-    document.getElementById('luz').src = pic;
-  }
